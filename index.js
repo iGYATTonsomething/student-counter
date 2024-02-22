9b960779a679eeea7181649f0731edaa8d63ec2d
zuko <146650432+iGYATTonsomething@users.noreply.github.com>
2024-02-22 12:27:17 +0800

    student-counter

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("previous-el")
+
+let count = 0
+
+function increment() {
+    count += 1
+
+    countEl.textContent = count
+}
+
+function save() {
+    let entries = count + " - "
+    saveEl.textContent += entries
+
+    count = 0
+    countEl.textContent = count
+}
